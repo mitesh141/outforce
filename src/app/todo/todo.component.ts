d415cd7a2952db360abb5354739bfe24ab10ad61
MITESH PARAB <71591343+mitesh141@users.noreply.github.com>
2023-10-18 00:02:36 +0530

    test

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -5,23 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent{
-title="todo";
-list:any[]=[];
+export class TodoComponent implements OnInit {
+  title = 'todo';
+  list: any[] = [];
 
-  addTask(item:string)
-  {
-    this.list.push({id:this.list.length,name:item})
-    console.warn(this.list);
-  }
-  addJob(job:string)
-  {
-    this.list.push({id:this.list.length,name:job})
-    console.warn(this.list);
-  }
-  removeTask(id:number){
-    console.warn(id)
-    this.list=this.list.filter(item=>item.id!==id);
+  ngOnInit() {
+    // Load tasks from local storage when the component initializes
+    const savedTasks = localStorage.getItem('tasks');
+    if (savedTasks) {
+      this.list = JSON.parse(savedTasks);
+    }
   }
 
+  addTask(item: string) {
+    const newTask = { id: this.list.length, name: item };
+    this.list.push(newTask);
+
+    // Save tasks to local storage
+    localStorage.setItem('tasks', JSON.stringify(this.list));
+  }
+
+  removeTask(id: number) {
+    console.warn(id);
+    this.list = this.list.filter(item => item.id !== id);
+
+    // Update tasks in local storage after removing a task
+    localStorage.setItem('tasks', JSON.stringify(this.list));
+  }
 }
